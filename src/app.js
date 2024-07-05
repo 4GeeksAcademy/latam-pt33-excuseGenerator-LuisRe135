739d69ceb52fe88c71af004f57e1987c2f790050
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-07-05 13:37:33 +0000

    first commit excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+
+let action = ["ate", "peed", "crushed", "broke"];
+
+let what = ["my homework", "my phone", "the car"];
+
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+const composedExcuse = (noun, verb, object, time) => {
+  let sentence =
+    noun[Math.floor(Math.random() * noun.length)] +
+    " " +
+    verb[Math.floor(Math.random() * verb.length)] +
+    " " +
+    object[Math.floor(Math.random() * object.length)] +
+    " " +
+    time[Math.floor(Math.random() * time.length)];
+  return sentence;
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = composedExcuse(
+    who,
+    action,
+    what,
+    when
+  );
 };
