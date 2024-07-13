b0428fe6730842546480546251f809be25d27485
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-07-13 02:42:51 +0000

    readability

--- src/app.js
+++ src/app.js
@@ -18,24 +18,22 @@ let when = [
   "during my lunch",
   "while I was praying"
 ];
-const composedExcuse = (noun, verb, object, time) => {
-  let sentence =
-    noun[Math.floor(Math.random() * noun.length)] +
-    " " +
-    verb[Math.floor(Math.random() * verb.length)] +
-    " " +
-    object[Math.floor(Math.random() * object.length)] +
-    " " +
-    time[Math.floor(Math.random() * time.length)];
-  return sentence;
-};
+
+function randonNumberGenerator(array) {
+  const randomNumber = Math.floor(Math.random() * array.length);
+  return array[randomNumber];
+}
+
+const excuse =
+  randonNumberGenerator(who) +
+  " " +
+  randonNumberGenerator(action) +
+  " " +
+  randonNumberGenerator(what) +
+  " " +
+  randonNumberGenerator(when);
 
 window.onload = function() {
   //write your code here
-  document.getElementById("excuse").innerHTML = composedExcuse(
-    who,
-    action,
-    what,
-    when
-  );
+  document.getElementById("excuse").innerHTML = excuse;
 };
